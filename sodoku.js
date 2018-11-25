bd775c0e86961fe04a6c6edd251692eefb4ca087
Frank Gutierrez <frank.gutierrez@samsclub.com>
2018-11-24 16:55:13 -0800

    remove comments

--- sodoku.js
+++ sodoku.js
@@ -16,75 +16,31 @@ const grid9 = [
 
 let solution = "None"; // grid solution
 
-/**
- * pretty prints grid to the console
- * @param  grid two dimensional array
- */
 const print_grid = grid => {
   const view = grid.map(row => row.map(x => (x ? x : " ")).join(" ")).join("\n");
   console.log(view);
 };
 
-/**
- * clone grid
- * @param  grid two dimensional array
- * @returns clone of grid
- */
 const copy_grid = grid => grid.map(row => [...row]);
 
-/**
- * determine if array contains unique values
- * @param  array
- * @return boolean
- */
 const isUniqueArray = array => array.length === new Set(array).size;
 
-/**
- * determine if subGrid has unique values
- * @param  grid two dimensional array
- * @return boolean
- */
 const hasUniqueValues = subGrid => {
   const oneDimensionalArray = subGrid.reduce((acc, row) => (!acc ? false : acc.concat(row)), []);
   const filteredArray = oneDimensionalArray.filter(x => x != 0);
   return isUniqueArray(filteredArray);
 };
 
-/**
- * transposeGrid by flipping the x and y axis
- * @param  grid two dimensional array
- * @return grid that has been transposed
- */
 const transposeGrid = grid => grid.map((_, row) => grid.map(col => col[row]));
 
-/**
- * determine if the grid rows are valid by containing unique values
- * @param  grid two dimensional array
- * @return boolean
- */
 const hasValidRows = grid =>
   grid.reduce((acc, row) => (!acc ? false : isUniqueArray([...row].filter(x => x !== 0))), true);
 
-/**
- * determine if the grid columns are valid by containing unique values
- * @param  grid two dimensional array
- * @return boolean
- */
 const hasValidColumns = grid => hasValidRows(transposeGrid(grid));
 
-/**
- * determine if subGrid is valid meaning valid row, columns and unique values
- * @param  grid two dimensional array
- * @return boolean
- */
 const isValidSubGrid = subGrid =>
   hasValidRows(subGrid) && hasValidColumns(subGrid) && hasUniqueValues(subGrid);
 
-/**
- * determine that sub-grids if any are valid
- * @param  grid two dimensional array
- * @return boolean
- */
 const hasValidSubGrids = (grid, n = grid.length) => {
   // bail if no subGrids
   const m = Math.trunc(n ** 0.5);
@@ -104,18 +60,8 @@ const hasValidSubGrids = (grid, n = grid.length) => {
   return subGrids.reduce((acc, subGrid) => (!acc ? false : isValidSubGrid(subGrid)), true);
 };
 
-/**
- * determine that the grid is valid
- * @param  grid two dimensional array
- * @return boolean
- */
 const isValidGrid = grid => hasValidRows(grid) && hasValidColumns(grid) && hasValidSubGrids(grid);
 
-/**
- * collect the spots that need to be filled
- * @param  grid two dimensional array
- * @return spots array of [x, y] coordinates
- */
 const getSpots = (grid, spots = []) => {
   grid.forEach((_, row) =>
     grid.forEach((_, col) => grid[row][col] === 0 && spots.push([row, col]))
@@ -123,19 +69,8 @@ const getSpots = (grid, spots = []) => {
   return spots;
 };
 
-/**
- * determine is sodoku is solved meaning no more spots to fill
- * @param  grid two dimensional array
- * @return boolean
- */
 const isSolved = grid => getSpots(grid).length === 0;
 
-/**
- * solve the sodoku grid
- * @param  grid two dimensional array
- * @param  spots array
- * @param  x value for spot
- */
 const solve = (grid, spots, x) => {
   // if all spots filled: stop searching
   if (spots.length === 0) return;
@@ -172,10 +107,6 @@ const printQuestion = (grid, spots, gridLength = grid.length, numOfSpots = spots
   console.log("-".repeat(gridLength * 2 - 1));
 };
 
-/**
- * run the main program
- * @param  grid two dimensional array
- */
 const main = (grid, n = grid.length) => {
   solution = "None";
 
